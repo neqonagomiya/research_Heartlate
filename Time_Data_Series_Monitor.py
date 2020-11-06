c55e866850fcc80287b230f3d418ae6d9649fd34
sora sato <sorasato@maudio8.el.tomakomai-ct.ac.jp>
2020-11-06 15:06:13 -0800

    Initial commit

--- Time_Data_Series_Monitor.py
+++ Time_Data_Series_Monitor.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 srl_HeartArdu = serial.Serial()
 srl_HeartArdu.baudrate = 9600
 srl_HeartArdu.port = /dev/cu.usbmodem14501 #this port is located typeC-typeA
+srl_HeartArdu.close()
 srl_HeartArdu.open()
 
 #setup port which use by "hand made measuring instrument"
 # i listen to Heart Rate from Ear
-srl_HRE = serial.Serial()
-srl_HRE.baudrate = 9600
-srl_HRE.port = /dev/cu.usbmodem14301 #this port is located the most left "usb type-A port"
-srl_HRE.open()
+#srl_HRE = serial.Serial()
+#srl_HRE.baudrate = 9600
+#srl_HRE.port = /dev/cu.usbmodem14301 #this port is located the most left "usb type-A port"
+#srl_HRE.open()
 
