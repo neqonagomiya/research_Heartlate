703b2d1265a0a2d52ff4c7f962265b46c62aa306
sora sato <sorasato@maudio8.el.tomakomai-ct.ac.jp>
2020-12-01 12:08:36 -0800

    Initial commit

--- Time_Data_Series_Monitor.py
+++ Time_Data_Series_Monitor.py
@@ -1,12 +1,18 @@
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+#from matplotlib.widgets import Button #we can use Button widgets in matplotlib
+#from matplotlib import animation
+#import time 
+#import sys 
 
 #setup port which use by "Arduino + HeartRateModule"
 #srl is serial port
 srl_HeartArdu = serial.Serial()
-srl_HeartArdu.baudrate = 9600
+srl_HeartArdu.baudrate = 9600 #arduinoと合わせる必要がある
 srl_HeartArdu.port = /dev/cu.usbmodem14501 #this port is located typeC-typeA
+#srl_HeartArdu.setDTR(False) #DTRを常にLOWにしReset阻止これがないと通信開始時にArduinoがリセットされて通信エラーになる可能性あり
 srl_HeartArdu.close()
 srl_HeartArdu.open()
 
@@ -17,3 +23,8 @@ srl_HeartArdu.open()
 #srl_HRE.port = /dev/cu.usbmodem14301 #this port is located the most left "usb type-A port"
 #srl_HRE.open()
 
+#csvに出力するための「arduinoからシリアル入力データを全て格納する変数」
+srl_HeartArdu_totaldata = []
+srl_HRE_totaldata = []
+
+#
