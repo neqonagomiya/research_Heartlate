20ef40bcbc0ffc86e1cc937447d1f55aff8287b0
sora sato <sorasato@maudio8.el.tomakomai-ct.ac.jp>
2020-12-03 16:49:03 -0800

    Initial commit

--- fft_Heart.py
+++ fft_Heart.py
@@ -15,12 +15,17 @@ f2 = 30
 #t = np.arange(0.N*dt,dt)
 
 # filter_LPF = signal.firwin(numtaps=1000,cutoff=f2,fs=sample_freq,pass_zero=False)
-filter_LPF = signal.firwin(numtaps=1000,cutoff=[1,f2],fs=sample_freq,pass_zero=False)
+filter_LPF = signal.firwin(numtaps=1000,cutoff=[0.3,f2],fs=sample_freq,pass_zero=False)
+
 y1 = signal.lfilter(filter_LPF,1,df)
 FFT = np.fft.fft(y1)
 df_FFT = np.fft.fft(df)
 
 freq = np.fft.fftfreq(len(FFT),dt)
+
+plt.plot(filter_LPF)
+plt.legend()
+plt.show()
 
 plt.plot(y1, label="filtered")
 plt.plot(df, label="original")
