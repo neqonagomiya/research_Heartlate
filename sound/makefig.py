1d94a7fc3cf28133a6c950f67f4c3739491f50e9
sora sato <sorasato@maudio8.el.tomakomai-ct.ac.jp>
2021-01-19 17:34:41 +0900

    makefig.py fix

--- sound/makefig.py
+++ sound/makefig.py
@@ -39,6 +39,20 @@ frames = np.loadtxt(select_filename,dtype='int' ,delimiter=',')
 CHANNELS = int(input("MONO: 1, STEREO: 2 >> "))
 SAMPLERATE = 48000
 
+#setting plot param
+plt.rcParams["xtick.direction"] = "in"               
+plt.rcParams["ytick.direction"] = "in"               
+plt.rcParams["xtick.minor.visible"] = True           
+plt.rcParams["ytick.minor.visible"] = True           
+plt.rcParams["xtick.major.width"] = 1             
+plt.rcParams["ytick.major.width"] = 1          
+plt.rcParams["xtick.minor.width"] = 0.5              
+plt.rcParams["ytick.minor.width"] = 0.5              
+plt.rcParams["xtick.major.size"] = 5                
+plt.rcParams["ytick.major.size"] = 5                
+plt.rcParams["xtick.minor.size"] = 2.5                 
+plt.rcParams["ytick.minor.size"] = 2.5               
+
 if CHANNELS == 1:
     frames_abs = np.abs(frames)
     maxval = np.amax(frames_abs)
@@ -54,22 +68,24 @@ if CHANNELS == 1:
     print('time',(len(frames_plot)/CHANNELS/SAMPLERATE))
     print('len(frames)/SAMPLERATE',(len(frames_plot)/SAMPLERATE))
     time = len(frames_plot)/CHANNELS/SAMPLERATE
-
-    #setting plot
-    plt.rcParams["xtick.direction"] = "in"
-    plt.rcParams["ytick.direction"] = "in"
     
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
+
+    fig0 = plt.figure()
+    ax1 = fig0.add_subplot(111)
     t = np.arange(start=0, stop=time,step=(1/SAMPLERATE))
     t = t[:-1]
     #setting axis
     ax1.set_xlabel("Time [s]")
     ax1.set_ylabel("Amplitude")
-    ax1.set_xlim([0,time+0.1])
+    #ax1.set_xlim([0,time+0.1])
+    ax1.set_xlim([0,3.0])
     ax1.grid(which='major',axis='both',color='#666666',alpha=0.25,linestyle=':',linewidth=1)
 
     ax1.plot(t,frames_plot)
+
+    fig0.tight_layout()
+    plt.show()
+
 ################################################
 else:
 
@@ -83,6 +99,54 @@ else:
     maxval_R = np.amax(frames_R_abs)
     frames_R_plot = frames_R/maxval_R
     
+    min_val_L = 0
+    com_val_L = 0
+    for i in range(0,38400):
+        com_val_L = frames_L_plot[i]
+        if min_val_L > com_val_L:
+            min_val_L = com_val_L
+            min_loc_L = i
+        else:
+            continue
+    
+    min_val_R = 0
+    com_val_R = 0
+ 
+    for i in range(0,38400):
+        com_val_R = frames_R_plot[i]
+        if min_val_R > com_val_R:
+            min_val_R = com_val_R
+            min_loc_R = i
+        else:
+            continue
+
+
+    print("min_loc_L:" ,min_loc_L)
+    print("min_loc_L numpy:",np.argmin(frames_L_plot[0:38400]))
+    print("min_val_L:", min_val_L)
+
+    print("min_loc_R:" ,min_loc_R)
+    print("min_loc_R numpy:",np.argmin(frames_R_plot[0:38400]))
+    print("min_val_R:", min_val_R)
+
+    ########################################################################################
+    # ECG　心拍間隔測定
+    ########################################################################################
+    min2_val_L = 0
+    com_val_L = 0
+    for i in range(48000,96000):
+        com_val_L = frames_L_plot[i]
+        if min2_val_L > com_val_L:
+            min2_val_L = com_val_L
+            min2_loc_L = i
+        else:
+            continue
+
+    print("min2_loc_L: ", min2_loc_L)
+    print("min2_loc_L_numpy: ", np.argmin(frames_L_plot[48000:96000]))
+    print("sample difference:",(min2_loc_L)-(min_loc_L))
+    ########################################################################################
+
     print(frames)
     print("frames type:",type(frames))
     print('size',len(frames))
@@ -93,53 +157,108 @@ else:
     print('time',(len(frames)/CHANNELS/SAMPLERATE))
     print('len(frames)/SAMPLERATE',(len(frames)/SAMPLERATE))
     time = len(frames)/CHANNELS/SAMPLERATE
-
-
-    #setting plot
-    plt.rcParams["xtick.direction"] = "in"
-    plt.rcParams["ytick.direction"] = "in"
- 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212)
     t = np.arange(start=0, stop=time,step=(1/SAMPLERATE))
-    #t = t[:-1]
+    t = t[:-1]
+    
+    with open(os.path.dirname(select_filename)+"param.txt","w") as f:
+        print("param",file=f)
+        print("CHANNNEL: "+str(CHANNELS),file=f)
+        print("min_loc_L:" + str(min_loc_L),file=f)
+        print("min_loc_L numpy:"+str(np.argmin(frames_L_plot[0:38400])),file=f)
+        print("min_val_L: "+str(min_val_L),file=f)
+        print("min_loc_R: "+str(min_loc_R),file=f)
+        print("min_loc_R numpy:"+str(np.argmin(frames_R_plot[0:38400])),file=f)
+        print("min_val_R:"+ str(min_val_R),file=f)
+        #print(frames)
+        print("frames type: "+str(type(frames)),file=f)
+        print('size: '+str(len(frames)),file=f)
+        print("MAXVALUE_L: "+str(maxval_L),file=f)
+        print("MAXVALUE_R: "+str(maxval_R),file=f)
+        print('time: '+str((len(frames)/CHANNELS/SAMPLERATE)), file=f)
+        print('len(frames)/SAMPLERATE: '+str((len(frames)/SAMPLERATE)),file=f)
+        print('timelen(frames)/CHANNELS/SAMPLERATE: '+ str(time), file=f)
+    
+    
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(211)
+    ax2 = fig1.add_subplot(212)
+
     #setting axis
     ax1.grid(which='major',axis='both',color='#666666',alpha=0.25,linestyle=':',linewidth=1)
     ax2.grid(which='major',axis='both',color='#666666',alpha=0.25,linestyle=':',linewidth=1)
     
     ax1.set_xlabel("Time [s]")
     ax1.set_ylabel("Amplitude")
-    ax1.set_xlim([0,time+0.1])
+    ax1.set_xlim([0,3.0])
     ax1.set_ylim([-1.1,1.1])
     ax1.set_title('(a) ECG',y=-0.45)
-
+   
     ax2.set_xlabel("Time [s]")
     ax2.set_ylabel("Amplitude")
-    ax2.set_xlim([0,time+0.1])
+    ax2.set_xlim([0,3.0])
     ax2.set_ylim([-1.1,1.1])
     ax2.set_title('(b) Mic output',y=-0.45)
 
+    ##########################################################################################
+    ax1.vlines(min_loc_L/SAMPLERATE,-1.1,1.1,colors='#333333',linestyle='dashed',linewidth=2)    
+    ax1.vlines(min2_loc_L/SAMPLERATE,-1.1,1.1,colors='#333333',linestyle='dashed',linewidth=2)
+    ax2.vlines(min_loc_R/SAMPLERATE,-1.1,1.1,colors='#333333',linestyle='dashed',linewidth=2)
+    ##########################################################################################
+
     ax1.plot(t,frames_L_plot,color='#ff4500')
     ax2.plot(t,frames_R_plot)
+    
+    fig1.tight_layout()
+    plt.show()
+
+    #enlarged
+    fig2 = plt.figure()
+    ax3 = fig2.add_subplot(211)
+    ax4 = fig2.add_subplot(212)
+
+    ax3.grid(which='major',axis='both',color='#666666',alpha=0.25,linestyle=':',linewidth=1)
+    ax4.grid(which='major',axis='both',color='#666666',alpha=0.25,linestyle=':',linewidth=1)
+    
+    ax3.set_xlabel("Time [s]")
+    ax3.set_ylabel("Amplitude")
+    ax3.set_xlim([0.5,1.0])
+    ax3.set_ylim([-1.1,1.1])
+    ax3.set_title('(a) ECG',y=-0.45)
+
+    ax4.set_xlabel("Time [s]")
+    ax4.set_ylabel("Amplitude")
+    ax4.set_xlim([0.5,1.0])
+    ax4.set_ylim([-1.1,1.1])
+    ax4.set_title('(b) Mic output',y=-0.45)
+
+    #########################################################################################
+    # ax3.vlines(min_loc_L/SAMPLERATE,-1.1,1.1,colors='#333333',linestyle='dashed',linewidth=2)
+    # ax4.vlines(min_loc_R/SAMPLERATE,-1.1,1.1,colors='#333333',linestyle='dashed',linewidth=2)
+    ##########################################################################################
+
+    ax3.plot(t,frames_L_plot,color='#ff4500')
+    ax4.plot(t,frames_R_plot)
+    
+    fig2.tight_layout()
+    plt.show()
 
 
-fig.tight_layout()
-plt.show()
 
 okflag = input('保存する? y or n >> ')
 if okflag=="y" or okflag=="": 
     dirpath = os.path.dirname(select_filename)
     print(dirpath,' << ここに保存するね')
     if CHANNELS==1:
-        fig.savefig(select_filename+'plot.png')
+        fig0.savefig(os.path.dirname(select_filename)+'plot.png')
     elif CHANNELS==2:
-        fig.savefig(select_filename+'plot.png')
-        extent1 = ax1.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-        extent2 = ax2.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        fig1.savefig(os.path.dirname(select_filename)+'plot.png')
+        extent1 = ax1.get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
+        extent2 = ax2.get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
         #サブプロットの周りの領域は、x方向の10%、y方向の20%で埋める。 
-        fig.savefig(select_filename+'plot_L.png',bbox_inches=extent1.expanded(1.17, 1.3))
-        fig.savefig(select_filename+'plot_R.png',bbox_inches=extent2.expanded(1.15, 1.3))
+        fig1.savefig(os.path.dirname(select_filename)+'plot_L.png',bbox_inches=extent1.expanded(1.17, 1.3))
+        fig1.savefig(os.path.dirname(select_filename)+'plot_R.png',bbox_inches=extent2.expanded(1.15, 1.3))
+        
+        fig2.savefig(os.path.dirname(select_filename)+'enlarged.png')
     else:
         print("対応してないよ")
         sys.exit()
